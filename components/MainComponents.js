a58af0f1dd7f65253576777a07a4ad0086a26160
Hannah Stover <hrosestover@gmail.com>
2019-12-05 13:46:32 -0800

    Forms

--- components/MainComponents.js
+++ components/MainComponents.js
@@ -4,6 +4,7 @@ import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import Reservation from "./ReservationComponent";
 import {
   View,
   Platform,
@@ -26,6 +27,31 @@ import {
   fetchPromos,
   fetchLeaders
 } from "../redux/ActionCreators";
+
+const ReservationNavigator = createStackNavigator(
+  {
+    Reservation: { screen: Reservation }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff",
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={24}
+          color="white"
+          onPress={() => navigation.toggleDrawer()}
+        />
+      )
+    })
+  }
+);
 
 const mapStateToProps = state => {
   return {
@@ -220,6 +246,21 @@ const MainNavigator = createDrawerNavigator(
             name="address-card"
             type="font-awesome"
             size={22}
+            color={tintColor}
+          />
+        )
+      }
+    },
+    Reservation: {
+      screen: ReservationNavigator,
+      navigationOptions: {
+        title: "Reserve Table",
+        drawerLabel: "Reserve Table",
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name="cutlery"
+            type="font-awesome"
+            size={24}
             color={tintColor}
           />
         )
