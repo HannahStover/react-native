f853fc52f66797e34efa150e14af391fd342d009
Hannah Stover <hrosestover@gmail.com>
2019-12-09 13:30:49 -0800

    Delete Favorites

--- components/MainComponents.js
+++ components/MainComponents.js
@@ -5,6 +5,7 @@ import DishDetail from "./DishdetailComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoriteComponent";
 import {
   View,
   Platform,
@@ -199,6 +200,31 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff",
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={24}
+          color="white"
+          onPress={() => navigation.toggleDrawer()}
+        />
+      )
+    })
+  }
+);
+
 const MainNavigator = createDrawerNavigator(
   {
     Home: {
@@ -248,6 +274,16 @@ const MainNavigator = createDrawerNavigator(
             size={22}
             color={tintColor}
           />
+        )
+      }
+    },
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        title: "My Favorites",
+        drawerLabel: "My Favorites",
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon name="heart" type="font-awesome" size={24} color={tintColor} />
         )
       }
     },
